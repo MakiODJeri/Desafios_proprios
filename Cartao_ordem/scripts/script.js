0c5ffe1193ffd0f2ac1099759b81b84e97bd07c6
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-11-09 23:55:17 -0300

    finalizado o estilo para o desafio cartão Ordo Realitas

---
+++ Cartao_ordem/scripts/script.js
@@ -0,0 +1,21 @@
+var nome = document.getElementById('nome')
+var funcao = document.getElementById('funcao')
+var contato = document.getElementById('contato')
+var nascimento = document.getElementById('nascimento')
+var registro = document.getElementById('registro')
+var foto = document.getElementById('foto')
+function primeiro() {
+    
+}
+function segundo() {
+
+}
+function terceiro() {
+    
+}
+function quarto() {
+    
+}
+function quinto() {
+    
+}
