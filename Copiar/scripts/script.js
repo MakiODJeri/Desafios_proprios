e273ad752edf2666b7a4333a9f5f4b5d10de256f
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-11-24 11:16:48 -0300

    estudando

--- Copiar/scripts/script.js
+++ Copiar/scripts/script.js
@@ -12,29 +12,53 @@ const borda_botao=(el)=>{
 const sai_botao=(el)=>{
     botao.style.border = '2px dashed black'
 }
-const marcar=(el,iden)=>{
-    const l = iden
+const marcar=(el)=>{
     const i = el.target
     i.classList.add('marca')
     guarda.push(i.innerHTML)
+}
+const check=(el)=>{
+    const istr = el.target.innerHTML
+    let res
+    for (let c of guarda) {
+        if (istr == c) {
+            res = true
+            break
+        }
+    }
+    return res
+}
+const remover=(el)=>{
+    const i = el.target
+    const istr = el.target.innerHTML
+    i.classList.remove('marca')
+    for (let c in guarda) {
+        if (istr == guarda[c]) {
+            guarda.splice(c,1)
+        }
+    }
+    
 }
 const box = [...document.querySelectorAll('.mons')]
 const team = [...document.querySelectorAll('.monsteam')]
 const botao = document.querySelector('#botao_copiar')
 let guarda = []
 let c = 0
-box.map((el,iden)=>{
+box.map((el)=>{
     el.addEventListener('mouseenter',(el)=>{
         borda(el)
     })
     el.addEventListener('mouseleave',(el)=>{
         sai(el)
     })
-    el.addEventListener('dblclick',(el)=>{
-        if (guarda.length > 2) {
+    el.addEventListener('click',(el)=>{
+        ch = check(el)
+        if (ch) {
+            remover(el)
+        } else if (guarda.length > 2){
             window.alert('Você já marcou o máximo de "mons" para o seu time')
         } else {
-            marcar(el,iden)
+            marcar(el)
             console.log(guarda)
         }
     })
@@ -48,6 +72,8 @@ botao.addEventListener('mouseleave',()=>{
 botao.addEventListener('click',()=>{
     team.map((el)=>{
         el.innerHTML = guarda[c]
+        remover(el)
         c++
+
     })
 })
