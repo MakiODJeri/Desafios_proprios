9338a86fd577423c13293a29a5c20186584cc6ec
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-11-27 13:24:52 -0300

    estudando

--- Exercícios_de_estudo/codigos/exercicio19/exercicio19.js
+++ Exercícios_de_estudo/codigos/exercicio19/exercicio19.js
@@ -1,4 +1,33 @@
 const bau = document.querySelector('#bau')
-const caixas = [...document.querySelectorAll('.caixa')]
-console.log(bau)
-console.log(caixas)
+const botao = document.querySelector('#alhevo')
+//const caixas = [...document.querySelectorAll('.caixa')]
+//const tres = document.querySelector('#caixa03')
+//console.log('=================================================')
+//console.log(bau)
+//console.log(caixas)
+//console.log('=================================================')
+//console.log(bau.children[3])
+//console.log(bau.firstElementChild)
+//console.log(bau.lastElementChild)
+//console.log('=================================================')
+//console.log(tres.children)
+//console.log(tres.parentNode)
+//console.log(tres.nextElementSibling)
+//console.log('=================================================')
+botao.addEventListener('click',()=>{
+    bau.style.background = 'black'
+    bau.style.border = '4px solid grey'
+    let i = [...bau.children]
+    let c = 1
+    i.map((el)=>{ 
+        el.innerHTML = 'ALHEVO'
+        el.style.border = '2px solid grey'
+        if (c%2 >0) {
+            el.style.background = 'darkred'
+            c++
+        } else {
+            el.style.background = 'lightgrey'
+            c++
+        }
+    })
+})
