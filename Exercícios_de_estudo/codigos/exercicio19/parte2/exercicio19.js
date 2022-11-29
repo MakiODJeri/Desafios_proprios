17329043d8813da9e14487e5f26fccb0b0c05972
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-11-29 11:01:16 -0300

    Estilo perto do final para o exercício 21

--- Exercícios_de_estudo/codigos/exercicio19/parte2/exercicio19.js
+++ Exercícios_de_estudo/codigos/exercicio19/parte2/exercicio19.js
@@ -2,19 +2,13 @@
  const cores = ['cyan','purple','magenta','green','blue']
  const adci = document.querySelector('#adci')
  const rmvr = document.querySelector('#rmvr')
- let iden = 'a'
 adci.addEventListener('click',()=>{
     let tamh = bau.children.length
     if (tamh < 9) {
         let sqr = document.createElement('div')  
         let randc = Math.floor(Math.random() * (5 - 0) + 0)
-        sqr.style.width = '100px'
-        sqr.style.height = '100px'
-        sqr.style.border = '2px solid black'
         sqr.style.background = cores[randc]
-        sqr.id = iden
-        console.log(iden)
-        iden += 'a'
+        sqr.setAttribute('class','square')
         bau.appendChild(sqr)
     } else {
         window.alert('Máximo de item adicionados a caixa')
@@ -23,5 +17,9 @@ adci.addEventListener('click',()=>{
 rmvr.addEventListener('click',()=>{
     let tamh = bau.children.length
     if (tamh > 0) {
+        bau.removeChild(bau.lastChild)
+        
+    } else {
+        window.alert('Mínimo de items dentro da caixa')
     }
 })
