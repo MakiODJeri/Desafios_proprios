d0b0278c2ce48011c507da235685196bfbc9fd32
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-01 14:52:49 -0300

    quase o fim do desafio 21

--- Exercícios_de_estudo/codigos/exercicio21/exercicio21.js
+++ Exercícios_de_estudo/codigos/exercicio21/exercicio21.js
@@ -1,10 +1,12 @@
 const adci = document.querySelector('#adci')
 const rmvr = document.querySelector('#rmvr')
-const selc = document.querySelector('#selc')
 const cxtx = document.querySelector('#nome')
 const list = document.querySelector('#lista')
+const cima = document.querySelector('#cima')
+const baix = document.querySelector('#baix')
 let txtid = 'curs'
 let numid = 0
+let idite = 0
 const crtcursid=()=>{
     numid++
     return txtid + numid.toString()
@@ -33,9 +35,30 @@ const adclist=(nome)=>{
         item.appendChild(radi)
         item.setAttribute('class','cursos')
         console.log(item)
-        list.appendChild(item)  
+        adpos(item)
     }
 }
+
+const adpos=(item)=>{
+    const tmnh = [...list.children] 
+    if (document.querySelector('input[name="curs"]:checked')) {
+        const curpos = document.querySelector('input[name="curs"]:checked').parentNode
+        if (idite == 1) {
+            list.insertBefore(item, curpos)
+            console.log(list.children)
+        } else {
+            if (false) {
+            } else {
+                const prox = curpos.nextSibling
+                list.insertBefore(item, prox)
+            }
+        }
+    } else {
+        list.appendChild(item)
+    }
+
+}
+
 adci.addEventListener('click',()=>{
     const nome = cxtx.value
     cxtx.value = ''
@@ -49,11 +72,15 @@ adci.addEventListener('click',()=>{
         adclist(nome)
     }
 }) //adicionar um novo curso a lista
+
+
+
 const rmvlist=(nome)=>{
     const curs = [...document.querySelectorAll('.cursos')]  
     curs.filter((el)=>{
         if (el.innerText == nome) {
             list.removeChild(el)
+            numid--
         }
     })
 }
@@ -62,6 +89,8 @@ rmvr.addEventListener('click',()=>{
     const radioc = document.querySelector('input[name="curs"]:checked')
     if (curs.length > 0 && radioc) {
         const cur = document.querySelector('input[name="curs"]:checked').parentNode.innerText
+        console.log(cur)
+        console.log(radioc)
         rmvlist(cur)
     } else if (curs.length == 0) {
         window.alert('Você precisa adicionar algum curso a lista para remover.')
@@ -69,3 +98,9 @@ rmvr.addEventListener('click',()=>{
         window.alert('Marque algum dos cursos da lista a direita para remover.')
     }
 })//remover um dos cursos que estão na lista
+cima.addEventListener('click',()=>{
+    idite = 1
+})
+baix.addEventListener('click',()=>{
+    idite = 0
+})
