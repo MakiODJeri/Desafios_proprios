438edc5a695b0b3b199095d9940ff472ae4c8f49
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-03 14:13:30 -0300

    Vou testar fazer um estilo mais complexo

--- Exercícios_de_estudo/codigos/exercicio22/exercicio22.js
+++ Exercícios_de_estudo/codigos/exercicio22/exercicio22.js
@@ -4,13 +4,24 @@ const core = document.querySelector('#cor')
 const posi = document.querySelector('#posi')
 const items = [...document.querySelectorAll('.item')]
 let selecionado = ''
+let listslec = []
 
 items.map((el)=>{
     el.addEventListener('click',()=>{
-        selecionado = el
-        posi.innerText = el.innerText
+        if (listslec[0] != el) {
+            selecionado = el
+            selecionado.classList.add('marca')
+            listslec.push(el)
+            posi.innerText = el.innerText
+        }
+        if (listslec.length == 2) {
+            const rmvel = listslec[0]
+            rmvel.classList.remove('marca')
+            listslec.shift()
+        }
     })
 })
+
 adci.addEventListener('click',()=>{
     if (selecionado == '') {
         window.alert('Selecione Algum dos items na lista abaixo antes de clicar em alguma opção.')
@@ -20,11 +31,11 @@ adci.addEventListener('click',()=>{
         selecionado.style.color = 'white'
         selecionado.style.textShadow = '1px 1px 0px black'
     }
-})
+})//botão para adicionar uma cor
 rmvr.addEventListener('click',()=>{
     if (selecionado == '') {
         window.alert('Selecione Algum dos items na lista abaixo antes de clicar em alguma opção.')
     } else {
         selecionado.style.background = 'white'
     }
-})
+})//botão para remover uma cor
