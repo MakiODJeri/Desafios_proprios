198329830906911e7b8a70e72f596fc42acae92f
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-05 13:13:37 -0300

    Método find

--- Exercícios_de_estudo/codigos/exercicio24/exercicio24.js
+++ Exercícios_de_estudo/codigos/exercicio24/exercicio24.js
@@ -7,3 +7,23 @@ const maxnum = document.querySelector('#max')
 const minnum = document.querySelector('#min')
 const telamos = document.querySelector('#mostrar')
 let list = []
+let max = 0
+let min = 0
+mina.addEventListener('click', ()=>{
+    if (min < 100) {
+        min++
+    } else {
+        window.alert('O valor máximo aceito é 99.')
+    }
+})
+minv.addEventListener('click', ()=>{
+    if (min >= 0) {
+        min--
+    }
+})
+maxa.addEventListener('click', ()=>{
+    max++
+})
+maxx.addEventListener('click', ()=>{
+    max--
+})
