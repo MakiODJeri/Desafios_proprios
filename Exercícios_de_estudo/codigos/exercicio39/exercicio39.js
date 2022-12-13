fce4101eff622da8c61862521310636fdab348ee
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-13 15:04:37 -0300

    Aiai

---
+++ Exercícios_de_estudo/codigos/exercicio39/exercicio39.js
@@ -0,0 +1,11 @@
+const input_nome = document.querySelector('#nome')
+const input_material = document.querySelector('#material')
+const input_attack = document.querySelector('#attack_damage')
+const radio_sim = document.querySelector('#sim_encantada')
+const radio_nao = document.querySelector('#nao_encantada')
+const select_encantamentos = document.querySelector('#opcoes_encantamentos')
+const btn_enviar = document.querySelector('#enviar')
+const div_ferramentas = document.querySelector('#ferramentas')
+
+btn_enviar.addEventListener('click', ()=>{
+})
