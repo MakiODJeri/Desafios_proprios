cc8d8cb9d1a44376427516a80da0de71996a5d7b
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-16 13:36:07 -0300

    O que foi isso?

--- Tela_de_calculadora2/scripts/calculos.js
+++ Tela_de_calculadora2/scripts/calculos.js
@@ -4,7 +4,6 @@ let v1 = 0
 let op = undefined
 let v2 = 0
 let onoff = false
-
 const remover_zero=(str)=>{
     if (str[0] == 0) {
         return str.slice(1)
@@ -12,19 +11,24 @@ const remover_zero=(str)=>{
         return str
     }
 }
-
 const calcular=()=>{
-    if (op == 'D') {
-        return Number(v1) / Number(v2)
-    } else if (op == 'x') {
-        return Number(v1) * Number(v2)
-    } else if (op == '-') {
-        return Number(v1) - Number(v2)
+    return eval(`${v1} ${op} ${v2}`)
+}
+const remover_ult=(str)=>{
+    if (str.length > 1) {
+        return str.slice(0,-1)
     } else {
-        return Number(v1) + Number(v2) 
+        return 0
     }
 }
-
+const encontrar=(str,cf)=>{
+    if (str !== 0) {
+        str = [...str]
+        return str.includes('.')
+    } else {
+        return false
+    }
+}
 botoes.map((btn)=>{
    if (btn.innerHTML == 7 || btn.innerHTML == 8 || btn.innerHTML == 9 || btn.innerHTML == 4 || btn.innerHTML == 5 || btn.innerHTML == 6 || btn.innerHTML == 1 || btn.innerHTML == 2 || btn.innerHTML == 3 || btn.innerHTML == 0) {
     btn.addEventListener('click', ()=>{
@@ -40,7 +44,7 @@ botoes.map((btn)=>{
             }
         }
     })
-   } else if (btn.innerHTML == 'D' || btn.innerHTML == 'x' || btn.innerHTML == '-' || btn.innerHTML == '+') {
+   } else if (btn.innerHTML == '/' || btn.innerHTML == '*' || btn.innerHTML == '-' || btn.innerHTML == '+') {
     btn.addEventListener('click', ()=>{
         if (v2 == 0) {
             op = btn.innerHTML
@@ -63,7 +67,7 @@ botoes[0].addEventListener('click', ()=>{
         v2 = 0
     }
 })
-botoes[1].addEventListener('click', ()=>{
+botoes[3].addEventListener('click', ()=>{
     if (onoff == true) {
         onoff = false
         span_tela.innerHTML = ''
@@ -72,7 +76,42 @@ botoes[1].addEventListener('click', ()=>{
         v2 = 0
     }
 })
-botoes[16].addEventListener('click', ()=>{
+botoes[1].addEventListener('click', ()=>{
+    if (onoff == true) {
+        if (op == undefined && v2 == 0) {
+            navigator.clipboard.writeText(v1)
+        } else if (v2 == 0) {
+            navigator.clipboard.writeText(`${v1} ${op}`)
+        } else {
+            navigator.clipboard.writeText(`${v1} ${op} ${v2} = ${calcular()}`)
+        }
+    }
+})
+botoes[2].addEventListener('click', ()=>{
+    if (v2 == 0 && op == undefined) {
+        v1 = remover_ult(v1)
+        span_tela.innerHTML = v1
+    } else if (v2 == 0) {
+        op = undefined
+        span_tela.innerHTML = `${v1}`
+    } else {
+        v2 = remover_ult(v2)
+        span_tela.innerHTML = `${v1} ${op} ${v2}`
+    }
+})
+botoes[18].addEventListener('click', ()=>{
+    if (op == undefined) {
+        if (encontrar(v1,'.') != true) {
+            v1 += '.'
+            span_tela.innerHTML = v1
+    }} else {
+        if (encontrar(v2,'.') != true) {
+            v2 += '.'
+            span_tela.innerHTML = `${v1} ${op} ${v2}`
+        }
+    }
+})
+botoes[19].addEventListener('click', ()=>{
     if (v1.length > 0 && op.length > 0 && v2.length > 0) {
         let res = calcular()
         span_tela.innerHTML = res
