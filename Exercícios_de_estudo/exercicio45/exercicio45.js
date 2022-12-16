81d01560cbc7d728d7194762c1304104b4ae1585
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-16 15:05:42 -0300

    Lindo

---
+++ Exercícios_de_estudo/exercicio45/exercicio45.js
@@ -0,0 +1,27 @@
+const btn_aba = document.querySelector('#btn_aba')
+const cpas = [...document.querySelectorAll('.cpas_js')]
+let estado_aba = false
+btn_aba.addEventListener('click', ()=>{
+    if (estado_aba == false) {
+        cpas.map((el)=>{
+            el.style.display = 'inline-block'
+        })
+        estado_aba = true
+        btn_aba.innerHTML = 'Fechar'
+    } else {
+        cpas.map((el)=>{
+            el.style.display = 'none'
+        })
+        estado_aba = false
+        btn_aba.innerHTML = 'Abrir'
+    }
+})
+cpas[1].addEventListener('click', ()=>{
+    if (cpas[0].value.length > 0) {
+        navigator.clipboard.writeText(cpas[0].value)
+        cpas[0].value = ''
+    }
+})
+cpas[2].addEventListener('click', ()=>{
+    
+})
