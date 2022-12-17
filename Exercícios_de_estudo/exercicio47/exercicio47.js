3b2e718d9813b736fd962656c8ca9e0f9be2c6e0
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-17 16:00:30 -0300

    Ok

--- Exercícios_de_estudo/exercicio47/exercicio47.js
+++ Exercícios_de_estudo/exercicio47/exercicio47.js
@@ -1,27 +1,33 @@
 const span_msg = document.querySelector('#span_msg')
+const btn_promise = document.querySelector('#btn_promise')
 const body = document.querySelector('body')
-
-let promise =new Promise((res_ok,)=>{
-
-})
-
-let hour = 0
-let interval = 3000
 const setar_msg=(msg,id='')=>{
     span_msg.innerHTML = msg
     body.id = id
 }
+const saber_horario=()=>{
+    let promise =new Promise((resolve,reject)=>{
+        let hour = 0
+        let interval = 3000
+        setTimeout(()=>{
+            hour =new Date().getHours()
+            if (hour > 5 && hour < 13) {
+                resolve([`São exatamente ${hour} horas Bom dia`,'bom_dia'])
+            } else if (hour > 12 && hour < 18) {
+                resolve([`São exatamente ${hour} horas Boa tarde`,'boa_tarde','boa_tarde'])
+            } else {
+                resolve([`São exatamente ${hour} horas Boa noite`,'boa_noite','boa_noite'])
+            }
+        },interval)
+    })
+    return promise
+} 
 
-setTimeout(()=>{
-    hour =new Date().getHours()
-},interval)
-
-if (hour > 5 && hour < 13) {
-    setar_msg(`São exatamente ${hour} horas Bom dia`,'bom_dia')
-} else if (hour > 12 && hour < 18) {
-    setar_msg(`São exatamente ${hour} horas Boa tarde`,'boa_tarde')
-} else {
-    setar_msg(`São exatamente ${hour} horas Boa noite`,'boa_noite')
-}
-
-setar_msg('Processando...')
+btn_promise.addEventListener('click', ()=>{
+    setar_msg('Processando...')
+    btn_promise.style.display = 'none'
+    saber_horario()
+    .then((retun)=>{
+        setar_msg(retun[0],retun[1])
+    })
+})
