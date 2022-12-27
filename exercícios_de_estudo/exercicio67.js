b8b31ce9653e097acf495698126a7532c68a59b0
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-27 18:24:53 -0300

    salve isso

--- exercícios_de_estudo/exercicio67.js
+++ exercícios_de_estudo/exercicio67.js
@@ -1,8 +1,18 @@
-function userLogin(name='??') {
+/*const giveAccess = person => console.log(`Acesso garantido ao ${person.level} ${person.name}`)
+function Authenticate(verify) {
     let array = []
-    for (let i = 0; i < 1000000000; i++) {
+    for (let i = 0; i < verify; i++) {
         array.push(i)
     }
-    console.log(`Acesso garantido para ${name}`)
+    return true
 }
-userLogin('Roberto')
+function login(person,fn) {
+    if (person.level === 'Admin') {
+        fn(5000000)
+    } else if (person.level === 'User') {
+        fn(1000000)
+    }
+    giveAccess(person)
+}
+login({level:'Admin',name:'Tarik'},Authenticate)
+login({level:'User',name:'Mike'},Authenticate)*/
