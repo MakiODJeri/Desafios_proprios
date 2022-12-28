96f19bb57677d6890c3edd6af66cb682ca678e4d
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2022-12-28 14:53:43 -0300

    Nice

---
+++ exercícios_de_estudo/exercicio69.js
@@ -0,0 +1,18 @@
+const kidsList = [
+    {
+        name:'Lucas'
+    },
+    {
+        name:'Alfredo'
+    },
+    {
+        name:'Rosa'
+    },
+    {
+        name:'Sebastian'
+    },
+    {
+        name:'Lilian'
+    }
+]
+const addAgesForKids = childrenList.map( child => {})
