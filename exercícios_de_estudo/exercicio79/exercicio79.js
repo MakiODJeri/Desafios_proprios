b2a787b1bd580b680db72851810ea7bb267e9402
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2023-01-03 15:09:18 -0300

    eita

--- exercícios_de_estudo/exercicio79/exercicio79.js
+++ exercícios_de_estudo/exercicio79/exercicio79.js
@@ -1,6 +1,10 @@
 // JSON
-const example = '{"cpu":"6800u","memory":[16,"GigaBytes"],"storage":[64,"GigaBytes"]}'
+const exampleObj = {
+    cpu: '6800u',
+    memory: [16,'gigabyte'],
+    storage: [512,'gigabyte']
+} 
+const exampleJSON = JSON.stringify(exampleObj)//Obj to JSON
 console.log(exampleJSON)
-const exampleJSON = JSON.parse(example) 
-console.log(exampleJSON)
-const exampleStr = JSON.parse
+const exampleStr = JSON.parse(exampleJSON)//JSON to Obj
+console.log(exampleStr)
