6873d4b82c2101c1441b80b7cfc01f26c9304630
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2023-01-03 13:01:27 -0300

    au

---
+++ exercícios_de_estudo/exercicio79/exercicio79.js
@@ -0,0 +1,6 @@
+// JSON
+const example = '{"cpu":"6800u","memory":[16,"GigaBytes"],"storage":[64,"GigaBytes"]}'
+console.log(exampleJSON)
+const exampleJSON = JSON.parse(example) 
+console.log(exampleJSON)
+const exampleStr = JSON.parse
