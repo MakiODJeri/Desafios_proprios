8dcee8c344c0028aee62329700963aa1fbf1c063
Kauã O.S <Oliveiradesousakaua9@gmail.com>
2023-01-10 08:55:55 -0300

    legal

--- exercicio90.js
+++ exercicio90.js
@@ -1,11 +1,32 @@
-const values = [
+/*const values = [
     0,
     {},
     '',
     [],
     NaN,
     () => {}
-]
-const truthyValus = values.filter(value => {
-    if (value) return value
-})
+]*/
+//const truthyValues = values.filter(value => {
+//    if (value) return value
+//})
+
+//const truthyValues = values.filter(
+//    Boolean(value)
+//)
+/*const BoolTrue = value => Boolean(value)
+const BoolFalse = value => !Boolean(value)
+const truthyValues = values.filter(BoolTrue)*/
+
+/*const inputBirthdayYear = '2007'
+
+const getAge = birthdayYear => {
+    try {
+        if (!Number(birthdayYear)) throw 'Invalide type value for birthday year'
+        if (birthdayYear > new Date().getFullYear()) throw 'Invalide birthday year'
+        return new Date().getFullYear() - Number(birthdayYear)
+    } catch (error) {
+        console.log('Error:\n' + error + ', try again.')
+    }
+}
+const age = getAge(inputBirthdayYear)*/
+
